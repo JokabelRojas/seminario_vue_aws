46d45e20cfb9b722f528e5a2c88c418b0fd435a7
JokabelRojas <jsaune@senati.pe>
2025-11-07 18:19:38 -0500

    Subiendo nuevo proyecto seminario_vue_aws

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
-console.log(supabase)
+import Input from '@/components/ui/input/Input.vue';
+import Avatar from './components/ui/avatar/Avatar.vue';
+import AvatarImage from './components/ui/avatar/AvatarImage.vue';
+import AvatarFallback from './components/ui/avatar/AvatarFallback.vue';
+import DropdownMenu from './components/ui/dropdown-menu/DropdownMenu.vue';
+import DropdownMenuTrigger from './components/ui/dropdown-menu/DropdownMenuTrigger.vue';
+import DropdownMenuContent from './components/ui/dropdown-menu/DropdownMenuContent.vue';
+import DropdownMenuLabel from './components/ui/dropdown-menu/DropdownMenuLabel.vue';
+import DropdownMenuSeparator from './components/ui/dropdown-menu/DropdownMenuSeparator.vue';
+import DropdownMenuItem from './components/ui/dropdown-menu/DropdownMenuItem.vue';
+import { Icon} from "@iconify/vue";
+
 </script>
 
 <template>
   <main>
-    <router-view/>
+    <nav class="h-16 border-b bg-muted/40 flex gap-4 justify-between px-8 items-center">
+      <form class="w-full max-w-96 flex items-center justify-center gap-3" 
+       action="">
+        <Input class="w-full pl-8 bg-background " />
+        <Icon icon="oui:search"  class="text-red-400 size-6.5" />
+        
+      </form>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar class="w-8 h-8 rounded-full bg-amber-400">
+            <AvatarImage
+              src="https://cdn.hashnode.com/res/hashnode/image/upload/v1717376548147/76aa248f-d783-4850-9ada-faab2ae3d5de.png"
+              alt="Jhonatan" />
+            <AvatarFallback>JH</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+          <DropdownMenuSeparator/>
+          <DropdownMenuItem>Mi perfil</DropdownMenuItem>
+          <DropdownMenuItem>Mi compras</DropdownMenuItem>
+          <DropdownMenuItem>Mi plan</DropdownMenuItem>
+          <DropdownMenuItem>Cerrar Sesion</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+
+
+    </nav>
+    <router-view />
   </main>
-  
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/carrera/index.vue
+++ src/pages/carrera/index.vue
@@ -1,11 +1,148 @@
-<template>
+<template >
     <div>
         <h1>Carreras</h1>
         <RouterLink :to="{name:'/'}"> Ir al dashboard </RouterLink>
+
+       
     </div>
+
+    <Table class="container m-10 ">
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]">
+          Id
+        </TableHead>
+        <TableHead>Nombre</TableHead>
+        <TableHead>Estado</TableHead>
+        <TableHead>Acciones</TableHead>   
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="car in carreras2" :key="car.id">
+        <TableCell class="font-medium">
+          {{ car.id }}
+        </TableCell>
+        <TableCell>{{ car.name }}</TableCell>
+        <TableCell>{{ car.status }}</TableCell>
+        <TableCell class="text-right flex gap-4">
+          
+        <Button variant="destructive">
+            <Icon icon="tdesign:delete-filled"  />
+        </Button>
+        
+        <Button variant="link" >
+            <Icon icon="oui:document-edit" />
+        </Button>
+
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+
+
+
+<div class="flex flex-row items-center justify-center">
+  <Pagination v-model="page2" :total="total" :items-per-page="perPage">
+    <PaginationContent v-slot="{ items }">
+      <PaginationPrevious @click="changePage3(page2)"/>
+
+     
+
+      <PaginationNext @click="changePage2(page2)"/>
+    </PaginationContent>
+  </Pagination>
+
+  <Select>
+    <SelectTrigger class="w-[70px]">
+      <SelectValue placeholder="Items" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        
+        <SelectItem value="apple">
+          5
+        </SelectItem>
+        <SelectItem value="banana">
+          10
+        </SelectItem>
+        <SelectItem value="blueberry">
+          15
+        </SelectItem>
+        
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</div>
 </template>
 
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient';
+import {  onMounted, ref, watch } from 'vue';
+import { Icon} from "@iconify/vue";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { Button } from '@/components/ui/button';
+
+
+const page2  = ref<number>(1)
+const perPage = ref<number>(10)
+
+const carreras2 = ref<any[]>([]);
+const total = ref<number>(0)
+
+const loadCarreras = async()=>{
+    const from =(page2.value -1) * perPage.value
+    const to = page2.value * perPage.value-1
+
+    const {data, error, count} = await supabase
+        .from('carrera')
+        .select("*",{count:"exact"})
+        .range(from,to)
+        .order("id",{ascending:true})
+
+    if (error){
+        console.log("Error de conexion ", error)
+        return
+    }
+    carreras2.value = data
+    total.value = count??0   
+}
+
+const changePage2 = (page:number)=>{
+    page2.value = page2.value +1
+    console.log(page2)
+}
+
+const changePage3 = (page:number)=>{
+    page2.value = page2.value -1
+    console.log(page2)
+}
+    onMounted(loadCarreras)
+    watch(page2, loadCarreras)
 
 </script>
 
